35d641ea63ef0d93d078f24a5834b40a356def45
Yoshiaki Oshima <oshima@buildman.co.jp>
2021-07-17 16:27:39 +0900

    init

---
+++ claims.js
@@ -0,0 +1,12 @@
+const admin = require('firebase-admin')
+const serviceAccount = require('./service-account.json')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+admin.auth().setCustomUserClaims(process.argv[2], {
+  admin: true,
+}).then(() => {
+  console.log("SUCCESS\n");
+}).catch(() => {
+  console.log("ERROR\n");
+});
